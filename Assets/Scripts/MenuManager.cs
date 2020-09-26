d3d3d5cfc682e52a7d9db2e10a7d704da3825450
shli3 <44980375+shli3@users.noreply.github.com>
2020-09-26 17:10:42 -0400

    Created Main Menu Scene & Built

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+	public void triggerMenuBehavoir(int i){
+		switch(i){
+		default:
+		case(0):
+			SceneManager.LoadScene("Play");
+			break;
+		case(1):
+			Application.Quit();
+			#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+			#endif
+			break;
+		}
+	}
+}
