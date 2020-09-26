b285cd93c77857480cbd290747f6bd51ccd4d767
shli3 <44980375+shli3@users.noreply.github.com>
2020-09-26 13:18:32 -0400

    Added Sound Effect everytime tree is clicked

--- Assets/Scripts/ClickTree.cs
+++ Assets/Scripts/ClickTree.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class ClickTree : MonoBehaviour
 {
+
+    public AudioSource TreeSFX;
+
   public void ButtonClick(){
+      TreeSFX.Play();
       GlobalTree.TreeCount += 1;
   }
 }
