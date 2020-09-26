1fbd844e08dee5ce1a6ec9f22bc7591943b3f8a5
shli3 <44980375+shli3@users.noreply.github.com>
2020-09-26 11:09:57 -0400

    Added button and counting scripts

---
+++ Assets/Scripts/ClickTree.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickTree : MonoBehaviour
+{
+  public void ButtonClick(){
+      GlobalTree.TreeCount += 1;
+  }
+}
---
+++ Assets/Scripts/GlobalTree.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalTree : MonoBehaviour
+{
+
+    public static int TreeCount;
+    public GameObject TreeDisplay;
+    public int InnerTree;
+
+    // Update is called once per frame
+    void Update()
+    {
+        InnerTree = TreeCount;
+        TreeDisplay.GetComponent<Text>().text = "Trees: " + InnerTree;
+    }
+}
