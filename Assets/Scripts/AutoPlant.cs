35be4331d9262ce799fa209d5d6234f69dfc53aa
shli3 <44980375+shli3@users.noreply.github.com>
2020-09-26 18:40:11 -0400

    Stopped tree from highlighting when clicked + added auto tree planting script

---
+++ Assets/Scripts/AutoPlant.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoPlant : MonoBehaviour
+{
+
+    public bool ATreePlanted = false;
+    public static int TreeAdded = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ATreePlanted == false){
+            ATreePlanted = true;
+            StartCoroutine(AutoPlantTree());
+        }
+    }
+
+    IEnumerator AutoPlantTree(){
+        yield return new WaitForSeconds(60);
+        GlobalTree.TreeCount += TreeAdded;
+        ATreePlanted = false;
+    }
+}
