fe0d58a93b901cc515bfec094872d1b65b104fd0
shli3 <44980375+shli3@users.noreply.github.com>
2020-09-26 17:50:23 -0400

    Added tree bobbing animation

--- Assets/Scripts/ClickTree.cs
+++ Assets/Scripts/ClickTree.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClickTree : MonoBehaviour
 {
 
     public AudioSource TreeSFX;
+    public GameObject PlantTree;
 
   public void ButtonClick(){
       TreeSFX.Play();
+      PlantTree.GetComponent<Animation>().Play("TreeBob");
       GlobalTree.TreeCount += 1;
   }
 }
